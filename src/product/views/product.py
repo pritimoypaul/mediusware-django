c0f20204b45eb3204daf9b29c88646ea7ad7edcf
Pritimoy Paul <pritimoy97@gmail.com>
2022-11-09 12:53:19 +0600

    list view complete

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,7 @@
+from itertools import count
 from django.views import generic
 
-from product.models import Variant
+from product.models import Variant, Product, ProductVariant, ProductVariantPrice
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +12,17 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+
+class ListProductView(generic.TemplateView):
+    template_name = 'products/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ListProductView, self).get_context_data(**kwargs)
+        products = Product.objects.all()[0:2]
+        context['products'] = products
+        context['pro_first_id'] = products[0].id
+        context['pro_last_id'] = products[products.count() - 1].id
+        context['product_count'] = Product.objects.all().count()
         return context
